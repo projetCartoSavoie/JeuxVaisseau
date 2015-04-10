eb7e755ec10cd0b059923badda745e082adacb3b
Céline de Roland <celinederoland@itpassion.info>
2015-04-10 13:42:10 +0200

    makefile de base

--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 test: test.o
-	gcc -o test test.o -lSDL2
+	gcc -o test test.o -lSDL2 -lGL -lGLU
 
 hello.o: hello.c
 	gcc -o test.o test.c -Wall
--- test.c
+++ test.c
@@ -1,12 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <SDL2/SDL.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
 
 int main(int argc, char *argv[]) {
-    if (SDL_Init(SDL_INIT_VIDEO) == -1) {// Démarrage de la SDL. Si erreur alors...                          {
+    if (SDL_Init(SDL_INIT_VIDEO) == -1) {// Démarrage de la SDL. Si erreur alors...
         fprintf(stderr, "Erreur d'initialisation de la SDL : %s\n", SDL_GetError()); // Ecriture de l'erreur
         exit(EXIT_FAILURE); // On quitte le programme
     }
+    int* x = GC_malloc(5*sizeof(int));
     SDL_Quit();
     return EXIT_SUCCESS;
 }
