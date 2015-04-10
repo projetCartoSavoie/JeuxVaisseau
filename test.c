2133e50d0782e9cffc5ce07981757039c1ccafd9
Céline de Roland <celinederoland@itpassion.info>
2015-04-10 12:59:43 +0200

    initialisation

---
+++ test.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <SDL/SDL.h>
+ 
+int main(int argc, char *argv[]) {
+if (SDL_Init(SDL_INIT_VIDEO) == -1) // Démarrage de la SDL. Si erreur alors...                          {
+fprintf(stderr, "Erreur d'initialisation de la SDL : %s\n", SDL_GetError()); // Ecriture de l'erreur
+exit(EXIT_FAILURE); // On quitte le programme
+}
+ SDL_Quit();
+ 
+return EXIT_SUCCESS;
+}
